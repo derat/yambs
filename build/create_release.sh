633005c244ec44621d6020daaa7d11868ddefa54
Daniel Erat <dan@erat.org>
2022-11-07 10:55:04 -0400

    build: Add create_release.sh and create_release.yaml.
    Add a Cloud Build config and shell script for creating
    release archives for linux/amd64, darwin/amd64,
    darwin/arm64, and windows/amd64.

---
+++ build/create_release.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -e
+
+set -x
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <os> <arch> <version>" >&2
+  exit 2
+fi
+
+export GOOS=$1
+export GOARCH=$2
+
+deps=
+if [ "$GOOS" = windows ]; then
+  deps="zip"
+fi
+
+# Install dependencies here instead of in release.yaml since changes
+# outside of the workspace don't persist across build steps.
+if [ -n "$deps" ] && [ "$(id -u)" -eq 0 ]; then
+  apt-get update && apt-get install -y $deps
+fi
+
+# Strip off the leading 'v' from tag names like 'v0.1'.
+version=${3#v}
+
+go build -ldflags "-X main.version=${version}" ./cmd/yambs
+
+# Copy additional license to the base directory for the archive.
+cp -f seed/COPYING-enums.md .
+
+archive=yambs-${version}-${GOOS}-${GOARCH}
+files="README.md LICENSE COPYING-enums.md"
+if [ "$GOOS" = windows ]; then
+  zip "${archive}.zip" yambs.exe $files
+  rm yambs.exe
+else
+  tar -czvf "${archive}.tar.gz" yambs $files
+  rm yambs
+fi
+
+# Clean up the file we copied earlier.
+rm -f COPYING-enums.md
